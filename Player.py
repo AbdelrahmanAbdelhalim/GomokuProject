dee909a9e37bb86ce9ecb96a62e3f0317ca61763
AbdelrahmanAbdelhalim <56049226+AbdelrahmanAbdelhalim@users.noreply.github.com>
2021-03-03 12:44:08 +0000

    Finished min max, pending testing

--- Player.py
+++ Player.py
@@ -1,11 +1,31 @@
 from gomokuAgent import GomokuAgent
 import misc as mis
+import numpy as np
 class Player(GomokuAgent):
 	def __init__ (self,ID,BOARD_SIZE,X_IN_A_LINE):
 		GomokuAgent.__init__(self, ID, BOARD_SIZE, X_IN_A_LINE)
 
+	#return a tuple with the move
 	def move(self,board):
-		print(123)
+		actions = actions(self,board)
+		if self.ID == 1:
+			maxSoFar = -1000
+			actionToReturn = None
+			for action in actions:
+				maxForThisAction = maxFunct(self,makeMove(board,action))
+				if  maxForThisAction> maxSoFar:
+					actionToReturn = action
+					maxSoFar = maxForThisAction
+			return actionToReturn
+		if self.ID == -1:
+			minSoFar = 1000
+			actionToReturn = None
+			for action in actions:
+				minForThisAction = minFunct(self,makeMove(board,action))
+				if minForThisAction < minSoFar:
+					actionToReturn = action
+					minSoFar = minForThisAction
+			return actionToReturn
 
 	def terminalTest(self,board):
 		if mis.winningTest(self.ID,board,X_ON_A_LINE) or mis.winningTest(-1*self.ID,board,X_IN_A_LINE):
@@ -25,7 +45,30 @@ class Player(GomokuAgent):
 		legalMoves = []
 		for r in range(self.BOARD_SIZE):
 			for c in range(self.BOARD_SIZE):
-				if legalMove(board,[r,c]):
+				if mis.legalMove(board,[r,c]):
 					legalMoves.append([r,c])
 		return legalMoves
+
+	def makeMove(self,board,move):
+		newBoard = np.array(board)
+		newBoard[move[0]][move[1]] = self.ID
+		return newBoard
+
+	def maxFunct(self,board):
+		if terminalTest(self,board):
+			return utility(self,board)
+		legalMoves = actions(self,board)
+		mxx = -1000
+		for move in legalMoves:
+			mxx = max(mxx,minFunct(self,makeMove(board,action)))
+		return mxx
+
+	def minFunct(self,board):
+		if terminalTest(self,board):
+			return utility(self,board)
+		legalMoves = actions(self,board)
+		mnn = 1000
+		for move in legalMoves:
+			mnn = min(minn,maxFunt(self,makeMove(board,action)))
+		return mnn
 
