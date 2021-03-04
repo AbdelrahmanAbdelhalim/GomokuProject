71d7c7fb613a67c792804353fce8bdad41751ab8
AbdelrahmanAbdelhalim <abdelrahmanbahaaeldin@gmail.com>
2021-03-04 12:55:00 -0500

    I spent 84 years solving this stupid fucking bug with alpha beta pruning. The bastard is now working fine. Working on evaluation next

--- Player/player.py
+++ Player/player.py
@@ -1,6 +1,7 @@
 from gomokuAgent import GomokuAgent
 import misc as mis
 import numpy as np
+import random as rand
 class Player(GomokuAgent):
 	def __init__ (self,ID,BOARD_SIZE,X_IN_A_LINE):
 		GomokuAgent.__init__(self, ID, BOARD_SIZE, X_IN_A_LINE)
@@ -11,58 +12,62 @@ class Player(GomokuAgent):
 		if not 0 in board:
 			return True
 	
-	def utility(self,board):
+	def utility(self,board,depth):
 		if mis.winningTest(self.ID,board,self.X_IN_A_LINE):
-			return self.ID
-		elif mis.winningTest(-1*self.ID,board,self.X_IN_A_LINE):
-			return -1 * self.ID
-		else:
-			return 0.5
+			return self.ID 
+		if mis.winningTest((-1*self.ID),board,self.X_IN_A_LINE):	
+			return (self.ID*-1)
+		if not 0 in board:
+			return 0
+
 	def actions(self,board):
 		legalMoves = []
-		for r in range(self.BOARD_SIZE):
-			for c in range(self.BOARD_SIZE):
+		for r in range(0,self.BOARD_SIZE+1):
+			for c in range(0,self.BOARD_SIZE+1):
 				if mis.legalMove(board,(r,c)):
 					legalMoves.append((r,c))
 		return legalMoves
 
 	def makeMove(self,board,move,player):
-		newBoard = np.array(board)
-		newBoard[move[0]][move[1]] = player
+		newBoard = np.copy(board)
+		newBoard[move] = player
 		return newBoard
 
-	def maxFunct(self,board,modifier,alpha,beta):
+	def maxFunct(self,board,modifier,alpha,beta,depth):
 		if self.terminalTest(board):
-			return self.utility(board)
+			return self.utility(board,depth)
 		legalMoves = self.actions(board)
 		mxx = -1000
 		for move in legalMoves:
-			mxx = max(mxx,self.minFunct(self.makeMove(board,move,self.ID*modifier),modifier*-1,alpha,beta))
-			if mxx >= alpha:
+			newBoard = self.makeMove(board,move,self.ID*modifier)
+			mxx = max(mxx,self.minFunct(newBoard,modifier*-1,alpha,beta,depth+1))
+			if mxx >= beta:
 				return mxx
 			alpha = max(mxx,alpha)
 		return mxx
 
-	def minFunct(self,board,modifier,alpha,beta):
+	def minFunct(self,board,modifier,alpha,beta,depth):
 		if self.terminalTest(board):
-			return self.utility(board)
+			return self.utility(board,depth)
 		legalMoves = self.actions(board)
 		mnn = 1000
 		for move in legalMoves:
-			mnn = min(mnn,self.maxFunct(self.makeMove(board,move,self.ID*modifier),modifier*-1,alpha,beta))
-			if mnn <= beta:
+			newBoard = self.makeMove(board,move,self.ID*modifier)
+			mnn = min(mnn,self.maxFunct(newBoard,modifier*-1,alpha,beta,depth+1))
+			if mnn <= alpha:
 				return mnn
 			beta = min(mnn,beta)
 		return mnn
 
-        #return a tuple with the move
+    #return a tuple with the move
 	def move(self,board):
 		legalMoves = self.actions(board)
 		if self.ID == 1:
 			mxxSoFar = -1000
 			actionToReturn = None
 			for action in legalMoves:
-				mxxForThisAction = self.maxFunct(self.makeMove(board,action,self.ID),-1,-1000,1000)
+				newBoard = self.makeMove(board,action,self.ID)
+				mxxForThisAction = self.minFunct(newBoard,-1,-1000,1000,1)
 				if mxxForThisAction > mxxSoFar:
 					actionToReturn = action
 					mxxSoFar = mxxForThisAction
@@ -71,7 +76,8 @@ class Player(GomokuAgent):
 			minSoFar = 1000
 			actionToReturn = None
 			for action in legalMoves:
-				minForThisAction = self.minFunct(self.makeMove(board,action,self.ID),-1,-1000,1000)
+				newBoard = self.makeMove(board,action,self.ID)
+				minForThisAction = self.maxFunct(newBoard,-1,-1000,1000,1)
 				if minForThisAction < minSoFar:
 					actionToReturn = action
 					minSoFar = minForThisAction
--- README.md
+++ README.md
@@ -11,12 +11,16 @@ This coursework involves building an AI agent that can play the Gomoku Game. Thi
 -----------
 * minmax algorithm apparently is now working fine, however it seems that the winning test in the utility function takes a lot of time for over 3 stones, but works just fine for two stones. The two stones game is very trivial but the AI seems to win every time it starts first and wins when the random agent doesn't score a win accidentally every time which is a good indicator that minmax is working just fine
 * Therefore I will have to look into alpha beta pruning and test the result. It will be harder to determine if an error is coming about from the minmax algorithm or the alpha beta pruning however.
-
+------------
+* Pruning is now working
+* minimax was working just fine, I spent years debugging a stupid bug with Alpha Beta pruning
+* Working on evaluation function and depth cut off now to play on bigger board
+------------
 
 ## Work that needs to be done:
 * Fixing the minmax algoritm (DONE)
-* Writing algorithm for depth cut off and pruning
-* Further optimization and strategy
+* Writing algorithm for pruing (DONE)
+* Evaluation and Depth Cut Off
 
 ## Notes:
 * The coursework will be submitted as soon as the agent is able to play games on the full 11x11 board
