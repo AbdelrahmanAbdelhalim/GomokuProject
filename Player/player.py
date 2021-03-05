648c30992f6cf72538e8f3acae7cd3a73255f2e3
AbdelrahmanAbdelhalim <abdelrahmanbahaaeldin@gmail.com>
2021-03-04 21:46:07 -0500

    Depth Cut off with a simple Utility Function done and working

--- Player/player.py
+++ Player/player.py
@@ -11,14 +11,30 @@ class Player(GomokuAgent):
 			return True
 		if not 0 in board:
 			return True
-	
-	def utility(self,board,depth):
-		if mis.winningTest(self.ID,board,self.X_IN_A_LINE):
-			return self.ID 
-		if mis.winningTest((-1*self.ID),board,self.X_IN_A_LINE):	
-			return (self.ID*-1)
+
+	def utility(self,board):
+		if mis.winningTest(self.ID,board,5):
+			return self.ID*self.X_IN_A_LINE
+		if mis.winningTest((-1*self.ID),board,5):	
+			return (self.ID*-1)*self.X_IN_A_LINE
 		if not 0 in board:
-			return 0
+			return 0.5*self.X_IN_A_LINE
+
+	def testForThreeStones(self,board):
+		if self.terminalTest(board):
+			return self.utility(board)
+		if mis.winningTest(self.ID,board,3):
+			return True
+		if mis.winningTest(-1*self.ID,board,3):
+			return True
+		return False
+
+	def cutOffTest(self,board,depth):
+		if depth == 2:
+			return True
+		if self.testForThreeStones(board):
+			return True
+		return False
 
 	def actions(self,board):
 		legalMoves = []
@@ -28,14 +44,37 @@ class Player(GomokuAgent):
 					legalMoves.append((r,c))
 		return legalMoves
 
+	def evaluate(self,board):
+		if mis.winningTest(self.ID,board,3):
+			return self.ID*3
+		if mis.winningTest(-1*self.ID,board,3):
+			return -1*self.ID*3
+		else:
+			return 2*self.ID
+
 	def makeMove(self,board,move,player):
 		newBoard = np.copy(board)
 		newBoard[move] = player
 		return newBoard
+	def testSurroundings(self,board,move):
+#		[-1,-1][0,-1][-1,1][0,-1][0,1][1,-1][1,0][1,1]
+		for r in range(-1,2):
+			for c in range(-1,2):
+				if move[0]+r >= 0 and move[0]+r < self.BOARD_SIZE and move[1]+c >= 0 and move[1]+c < self.BOARD_SIZE:
+					squareToCheck = (move[0]+r,move[1]+c)
+					if board[squareToCheck] == 1 or board[squareToCheck] == -1:
+						return True
+		return False
+	def selectLegalMoves(self,board,legalMoves):
+		selectedLegalMoves = []
+		for move in legalMoves:
+			if self.testSurroundings(board,move):
+				selectedLegalMoves.append(move)
+		return selectedLegalMoves
 
 	def maxFunct(self,board,modifier,alpha,beta,depth):
-		if self.terminalTest(board):
-			return self.utility(board,depth)
+		if self.cutOffTest(board,depth):
+			return self.evaluate(board)
 		legalMoves = self.actions(board)
 		mxx = -1000
 		for move in legalMoves:
@@ -45,10 +84,10 @@ class Player(GomokuAgent):
 				return mxx
 			alpha = max(mxx,alpha)
 		return mxx
-
+		
 	def minFunct(self,board,modifier,alpha,beta,depth):
-		if self.terminalTest(board):
-			return self.utility(board,depth)
+		if self.cutOffTest(board,depth):
+			return self.evaluate(board)
 		legalMoves = self.actions(board)
 		mnn = 1000
 		for move in legalMoves:
@@ -61,7 +100,7 @@ class Player(GomokuAgent):
 
     #return a tuple with the move
 	def move(self,board):
-		legalMoves = self.actions(board)
+		legalMoves = self.selectLegalMoves(board,self.actions(board))
 		if self.ID == 1:
 			mxxSoFar = -1000
 			actionToReturn = None
